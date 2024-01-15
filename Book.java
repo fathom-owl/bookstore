bd76a933db6ac00b3fa9ee5dce42c0bb70576e6a
Lilly <152177775+fathom-owl@users.noreply.github.com>
2024-01-15 14:06:12 +0600

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,24 @@
+// Book.java
+public class Book {
+    private String isbn;
+    private String title;
+    private double price;
+
+    public Book(String isbn, String title, double price) {
+        this.isbn = isbn;
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
